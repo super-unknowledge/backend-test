58a1b69398ef7c2d1ee721dafeb11be3bf9268f6
super-unknowledge <64061479+super-unknowledge@users.noreply.github.com>
2025-08-30 11:38:22 +0800

    Connect sqlite db (empty)

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+from database import SessionLocal, Candidate
+
+def get_db():
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
 	return ("root")
+
+@app.get("/candidates")
+def read_candidates(db: Session = Depends(get_db)):
+	candidates = db.query(Candidate).all()
+	return candidates
