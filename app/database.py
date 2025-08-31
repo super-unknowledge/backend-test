3538236ec332cc1bce49707cebaa86cee17bde43
super-unknowledge <64061479+super-unknowledge@users.noreply.github.com>
2025-08-31 13:16:20 +0800

    Add database models

---
+++ app/database.py
@@ -0,0 +1,62 @@
+from sqlalchemy import (
+	ForeignKey,
+	JSON,
+	DateTime,
+	Enum,
+)
+from sqlalchemy.orm import (
+	DeclarativeBase, 
+	Mapped, 
+	mapped_column, 
+	relationship
+)
+from datetime import datetime
+import enum
+from typing import List
+
+
+class StatusEnum(enum.Enum):
+	APPLIED = "APPLIED"
+	INTERVIEWING = "INTERVIEWING"
+	REJECTED = "REJECTED"
+	HIRED = "HIRED"
+
+
+class Base(DeclarativeBase):
+	pass
+
+
+class Candidate(Base):
+	__tablename__ = "candidates"
+	id: Mapped[int] = mapped_column(
+		primary_key=True,
+	)
+	full_name: Mapped[str]
+	email: Mapped[str] = mapped_column(
+		unique=True,
+	)
+	phone: Mapped[str | None]
+	skills: Mapped[list[str]] = mapped_column(JSON)
+	created_at: Mapped[datetime] = mapped_column(
+		DateTime, default=datetime.utcnow
+	)
+
+
+class Application(Base):
+ 	__tablename__ = "applications"
+ 	id: Mapped[int] = mapped_column(
+ 		primary_key=True,
+ 	)
+ 	candidate_id: Mapped["Candidate"] = relationship(
+		back_populates="candidates"
+	)
+ 	job_title: Mapped[str]
+ 	status: Mapped[StatusEnum] = mapped_column(
+		Enum(StatusEnum, name="status_enum"),
+		nullable=False,
+		default=StatusEnum.APPLIED
+	)
+ 	applied_at: Mapped[datetime] = mapped_column(
+		DateTime, default=datetime.utcnow
+	)
+
