27df66a85ad7f1aed1f5facd9e72273c7d1987ea
super-unknowledge <64061479+super-unknowledge@users.noreply.github.com>
2026-01-05 23:45:49 +0800

    change db and endpoints to async

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from datetime import datetime, timezone
 import enum
 
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Relationship, Column, JSON, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Relationship, Column, JSON, Session, SQLModel, select
 from sqlalchemy import Enum as SQLEnum
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 
 class Status(str, enum.Enum):
@@ -74,60 +77,65 @@ class ApplicationUpdate(ApplicationBase):
 
 
 sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+sqlite_url = f"sqlite+aiosqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_async_engine(sqlite_url, connect_args=connect_args)
 
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
 
 
-def get_session():
-    with Session(engine) as session:
+async def get_async_session() -> AsyncSession:
+    async_session = sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with async_session() as session:
         yield session
 
 
-SessionDep = Annotated[Session, Depends(get_session)]
+AsyncSessionDep = Annotated[AsyncSession, Depends(get_async_session)]
 app = FastAPI()
 
 
 @app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+async def on_startup():
+    await create_db_and_tables()
 
 
 @app.post("/candidates/", response_model=CandidatePublic)
-def create_candidate(candidate: CandidateCreate, session: SessionDep):
+async def create_candidate(candidate: CandidateCreate, session: AsyncSessionDep):
     db_candidate = Candidate.model_validate(candidate)
     session.add(db_candidate)
-    session.commit()
-    session.refresh(db_candidate)
+    await session.commit()
+    await session.refresh(db_candidate)
     return db_candidate
 
 
 @app.post("/candidates/{candidate_id}/applications", response_model=ApplicationPublic)
-def create_application(
-        candidate_id: uuid.UUID,
-        application: ApplicationCreate,
-        session: SessionDep
+async def create_application(
+    candidate_id: uuid.UUID,
+    application: ApplicationCreate,
+    session: AsyncSessionDep
 ):
     application_data = application.model_dump()
     db_application = Application(**application_data, candidate_id=candidate_id)
     session.add(db_application)
-    session.commit()
-    session.refresh(db_application)
+    await session.commit()
+    await session.refresh(db_application)
     return db_application
 
 
 @app.get("/candidates/", response_model=list[CandidatePublic])
-def read_candidates(
-    session: SessionDep,
+async def read_candidates(
+    session: AsyncSessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
 ):
-    candidates = session.exec(select(Candidate).offset(offset).limit(limit)).all()
+    result = await session.exec(select(Candidate).offset(offset).limit(limit))
+    candidates = result.all()
     return candidates
 
 
@@ -135,13 +143,13 @@ def read_candidates(
     "/candidates/{candidate_id}/applications",
     response_model=list[ApplicationPublic]
 )
-def read_applications(
+async def read_applications(
     candidate_id: uuid.UUID,
-    session: SessionDep,
+    session: AsyncSessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
 ):
-    candidate = session.get(Candidate, candidate_id)
+    candidate = await session.get(Candidate, candidate_id)
     if not candidate:
         raise HTTPException(status_code=404, detail="Candidate not found")
 
@@ -151,48 +159,49 @@ def read_applications(
         .offset(offset)
         .limit(limit)
     )
-    applications = session.exec(statement).all()
+    result = await session.exec(statement)
+    applications = result.all()
 
     return applications
 
 
 @app.get("/candidates/{candidate_id}", response_model=CandidatePublic)
-def read_candidate(candidate_id: uuid.UUID, session: SessionDep):
-    candidate = session.get(Candidate, candidate_id)
+async def read_candidate(candidate_id: uuid.UUID, session: AsyncSessionDep):
+    candidate = await session.get(Candidate, candidate_id)
     if not candidate:
         raise HTTPException(status_code=404, detail="Candidate not found")
     return candidate
 
 
 @app.put("/candidates/{candidate_id}", response_model=CandidatePublic)
-def update_candidate(
-        candidate_id: uuid.UUID,
-        candidate: CandidateUpdate,
-        session: SessionDep
+async def update_candidate(
+    candidate_id: uuid.UUID,
+    candidate: CandidateUpdate,
+    session: AsyncSessionDep
 ):
-    candidate_db = session.get(Candidate, candidate_id)
+    candidate_db = await session.get(Candidate, candidate_id)
     if not candidate_db:
         raise HTTPException(status_code=404, detail="Candidate not found")
     candidate_data = candidate.model_dump()
     candidate_db.sqlmodel_update(candidate_data)
     session.add(candidate_db)
-    session.commit()
-    session.refresh(candidate_db)
+    await session.commit()
+    await session.refresh(candidate_db)
     return candidate_db
 
 
 @app.patch("/applications/{application_id}", response_model=ApplicationPublic)
-def update_application_status(
+async def update_application_status(
     application_id: uuid.UUID,
     application: ApplicationUpdate,
-    session: SessionDep
+    session: AsyncSessionDep
 ):
-    application_db = session.get(Application, application_id)
+    application_db = await session.get(Application, application_id)
     if not application_db:
         raise HTTPException(status_code=404, detail="Application not found")
     application_data = application.model_dump(exclude_unset=True)
     application_db.sqlmodel_update(application_data)
     session.add(application_db)
-    session.commit()
-    session.refresh(application_db)
+    await session.commit()
+    await session.refresh(application_db)
     return application_db
